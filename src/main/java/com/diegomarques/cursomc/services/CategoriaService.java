acdd7e7ed45dc4d69c9a192f5d5b9df2ac747fec
Diego Marques <jdiegomarquesr@icloud.com>
2019-04-17 00:17:17 -0300

    Ajuste no endpoint /categorias/{id}

--- src/main/java/com/diegomarques/cursomc/services/CategoriaService.java
+++ src/main/java/com/diegomarques/cursomc/services/CategoriaService.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.diegomarques.cursomc.domain.Categoria;
 import com.diegomarques.cursomc.repositories.CategoriaRepository;
+import com.diegomarques.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		}
-	
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
 }
